37e681ece4a34d7538c302e2a9bbabb50f0c73b1
Adam Jordan <adamyordan@gmail.com>
2017-05-11 00:39:39 +0700

    add raw data and combiner script

---
+++ preprocessing/combine_dumps.py
@@ -0,0 +1,18 @@
+import json
+
+output_file = 'dump.json'
+files = []
+events = []
+
+for i in range(0, 34 + 1):
+    files.append('raw/dump{:02d}.json'.format(i))
+
+for file in files:
+    with open(file) as f:
+        d = json.load(f)
+        events.extend(d['events'])
+
+print 'dumped %d events' % len(events)
+
+with open(output_file, 'w') as f:
+    json.dump(events, f)
