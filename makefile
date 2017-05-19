be0d91916c369dba9893f24995bef20d62443f0a
Geraldo <geraldo.w3096@gmail.com>
2017-05-19 17:01:26 +0700

    Cross Validate with all data, add makefile

---
+++ makefile
@@ -0,0 +1,24 @@
+clean:
+    rm --force preprocessing/*.csv
+    rm --force preprocessing/intent_summary.txt
+    rm --force feature_extraction/*.csv
+    rm --force feature_extraction/balanced_label feature_extraction/label
+    rm --force training/*.p
+preprocess:
+    python preprocessing/make_csv.py
+    python preprocessing/convert_intent.py
+generate_feature:
+    python feature_extraction/extract_feature.py
+train:
+    python training/training.py
+
+help:
+    @echo "    clean"
+    @echo "        clean all artifacts."
+    @echo "    preprocess"
+    @echo "        Build process artifacts."
+    @echo "    generate_feature"
+    @echo "        Generate Feature."
+    @echo "    train"
+    @echo "        Do training."
+
--- training/training.py
+++ training/training.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import numpy as np
-
-#from sklearn.feature_selection import SelectFromModel
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.feature_selection import SelectFromModel
+import pickle
+from sys import argv
+target = open("metadata", 'w')
 #label
+
 X = pd.read_csv('../feature_extraction/feature_balanced.csv')
-y = pd.read_csv('../feature_extraction/balanced_label.csv')
+y = pd.read_csv('../feature_extraction/balanced_label').values.ravel() #Force LOL
+
+X_extra = pd.read_csv('../feature_extraction/feature_freq.csv')
+y_extra = pd.read_csv('../feature_extraction/label').values.ravel() #Force LOL
 
 # #remove extra feature -- ExtraTrees
 #select=ExtraTreesClassifier()
@@ -21,15 +30,36 @@ y = pd.read_csv('../feature_extraction/balanced_label.csv')
 from sklearn.naive_bayes import BernoulliNB
 bnb = BernoulliNB()
 bnb.fit(X,y)
-bnb.score(X,y)
+target.write("BNB Balanced Data score: ")
+target.write(repr(bnb.score(X,y)))
+target.write("\n")
+
 predicted = cross_val_predict(bnb, X, y, cv=10)
-print(metrics.accuracy_score(y, predicted))
-pickle.dump(mlp, open("BNBAllFeature.p","wb"))
+target.write("BNB Balanced Data 10-Cross fold: ")
+target.write(repr(metrics.accuracy_score(y, predicted)))
+target.write("\n")
+
+predicted = cross_val_predict(bnb, X_extra, y_extra, cv=10)
+target.write("BNB Balanced Data 10-Cross fold All Data: ")
+target.write(repr(metrics.accuracy_score(y_extra, predicted)))
+target.write("\n")
+pickle.dump(bnb, open("BNBAllFeature.p","wb"))
 
 from sklearn.neural_network import MLPClassifier
 mlp = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(25, 100))
 mlp.fit(X,y)
-mlp.score(X,y)
+target.write("MLP Balanced Data score: ")
+target.write(repr(mlp.score(X,y)))
+target.write("\n")
+
 predicted = cross_val_predict(mlp, X, y, cv=10)
-print(metrics.accuracy_score(y, predicted))
+target.write("MLP Balanced Data 10-Cross : ")
+target.write(repr(metrics.accuracy_score(y, predicted)))
+target.write("\n")
+
+predicted = cross_val_predict(mlp, X_extra, y_extra, cv=10)
+target.write("MLP Balanced Data 10-Cross Extra All Data: ")
+target.write(repr(metrics.accuracy_score(y_extra, predicted)))
+target.write("\n")
 pickle.dump(mlp, open("MLPAllFeature.p","wb"))
+target.close()
