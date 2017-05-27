f9cfef0cbc02522e704a81ac0c05a48844dcc1e7
Adam Jordan <adamyordan@gmail.com>
2017-05-27 15:27:41 +0700

    added selected and nonselected options in training

--- training_testing/assess.py
+++ training_testing/assess.py
@@ -1,37 +1,85 @@
 import pandas as pd
 import pickle
+import pickle
+from sklearn.model_selection import cross_val_score
 from sklearn import metrics
 
-test_data = pd.read_csv('test-dataset.csv')
+stopwords = open('../preprocessing/stopwords_id.txt', 'rb').read().splitlines()
+
+abbr_tuples = [ line.split(' = ') for line in open('../preprocessing/abbreviations_id.txt', 'rb').read().splitlines() ]
+abbreviations = { s[0] : s[1] for s in abbr_tuples }
+
+def remove_symbols(s):
+    return ''.join([c for c in s if c.isalnum() or c == ' '])
+
+def remove_numbers(s):
+    return ''.join([c for c in s if c.isalpha() or c == ' '])
+
+def normalize_abbreviations(s):
+    words = []
+    for w in s.split(' '):
+        words.append(abbreviations[w] if w in abbreviations else w)
+    return ' '.join(words)
+
+def remove_stopwords(s):
+    return ' '.join([w for w in s.split(' ') if w not in stopwords])
+
+def normalize_text(s):
+    s = s.lower()
+    s = remove_symbols(s)
+    s = remove_numbers(s)
+    s = normalize_abbreviations(s)
+    s = remove_stopwords(s)
+    return s
+
+def write_feature_to_file(filename, count_vect, feature):
+    df = pd.DataFrame(feature.toarray())
+    df.to_csv(filename, header = count_vect.get_feature_names(), index=False)
+
+test_data = pd.read_csv('test-dataset-new.csv')
+for i,row in test_data.iterrows():
+    test_data.loc[i,'text'] = normalize_text(str(test_data.loc[i,'text']))
+
+
+
 
 def gen_report(model, vectorizer, test_data):
     features = vectorizer.transform(test_data['text'])
+    print features.shape
     predicted = model.predict(features)
     report = metrics.classification_report(test_data['intent'], predicted)
+    c_mat = metrics.confusion_matrix(test_data['intent'], predicted)
+
     print '*' * 70
     print str(model)
     print '*' * 70
+    scores = cross_val_score(model, features, test_data['intent'], cv=5)
+    print scores
+    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    print c_mat
     print report
+    print
 
-MNB_freq_inb = pickle.load(open('output/MNB_freq_inb.pkl', 'rb'))
-MNB_bool_inb = pickle.load(open('output/MNB_bool_inb.pkl', 'rb'))
-MNB_freq_bal = pickle.load(open('output/MNB_freq_bal.pkl', 'rb'))
-MNB_bool_bal = pickle.load(open('output/MNB_bool_bal.pkl', 'rb'))
-tree_freq_inb = pickle.load(open('output/tree_freq_inb.pkl', 'rb'))
-tree_bool_inb = pickle.load(open('output/tree_bool_inb.pkl', 'rb'))
-tree_freq_bal = pickle.load(open('output/tree_freq_bal.pkl', 'rb'))
-tree_bool_bal = pickle.load(open('output/tree_bool_bal.pkl', 'rb'))
+vect_freq_selected = pickle.load( open('../feature_selection/output/vect_freq_selected.pkl', 'rb') )
+vect_freq_balanced_selected = pickle.load( open('../feature_selection/output/vect_freq_balanced_selected.pkl', 'rb') )
+vect_freq = pickle.load( open('../feature_extraction/output/vect_freq.pkl', 'rb') )
+vect_freq_balanced = pickle.load( open('../feature_extraction/output/vect_freq_balanced.pkl', 'rb') )
 
-vect_freq_selected = pickle.load(open('../feature_selection/output/vect_freq_selected.pkl', 'rb'))
-vect_bool_selected = pickle.load(open('../feature_selection/output/vect_bool_selected.pkl', 'rb'))
-vect_freq_balanced_selected = pickle.load(open('../feature_selection/output/vect_freq_balanced_selected.pkl', 'rb'))
-vect_bool_balanced_selected = pickle.load(open('../feature_selection/output/vect_bool_balanced_selected.pkl', 'rb'))
+MNB_freq_inb_s = pickle.load( open('output/MNB_freq_inb_s.pkl') )
+MNB_freq_inb = pickle.load( open('output/MNB_freq_inb.pkl') )
+MNB_freq_bal_s = pickle.load( open('output/MNB_freq_bal_s.pkl') )
+MNB_freq_bal = pickle.load( open('output/MNB_freq_bal.pkl') )
+tree_freq_inb_s = pickle.load( open('output/tree_freq_inb_s.pkl') )
+tree_freq_inb = pickle.load( open('output/tree_freq_inb.pkl') )
+tree_freq_bal_s = pickle.load( open('output/tree_freq_bal_s.pkl') )
+tree_freq_bal = pickle.load( open('output/tree_freq_bal.pkl') )
 
-gen_report(MNB_freq_inb, vect_freq_selected, test_data)
-gen_report(MNB_bool_inb, vect_bool_selected, test_data)
-gen_report(MNB_freq_bal, vect_freq_balanced_selected, test_data)
-gen_report(MNB_bool_bal, vect_freq_balanced_selected, test_data)
-gen_report(tree_freq_inb, vect_freq_selected, test_data)
-gen_report(tree_bool_inb, vect_bool_selected, test_data)
-gen_report(tree_freq_bal, vect_freq_balanced_selected, test_data)
-gen_report(tree_bool_bal, vect_freq_balanced_selected, test_data)
+gen_report(MNB_freq_inb_s, vect_freq_selected, test_data)
+gen_report(MNB_freq_inb, vect_freq, test_data)
+gen_report(MNB_freq_bal_s, vect_freq_balanced_selected, test_data)
+gen_report(MNB_freq_bal, vect_freq_balanced, test_data)
+
+gen_report(tree_freq_inb_s, vect_freq_selected, test_data)
+gen_report(tree_freq_inb, vect_freq, test_data)
+gen_report(tree_freq_bal_s, vect_freq_balanced_selected, test_data)
+gen_report(tree_freq_bal, vect_freq_balanced, test_data)
--- training_testing/training.py
+++ training_testing/training.py
@@ -6,33 +6,58 @@ import pickle
 def main():
 
     print "[+] Reading data from files"
-    dataset_freq_inb = pd.read_csv('../feature_selection/output/features_freq_selected.csv')
-    dataset_bool_inb = pd.read_csv('../feature_selection/output/features_bool_selected.csv')
-    dataset_freq_bal = pd.read_csv('../feature_selection/output/features_freq_balanced_selected.csv')
-    dataset_bool_bal = pd.read_csv('../feature_selection/output/features_bool_balanced_selected.csv')
+    dataset_freq_inb_s = pd.read_csv('../feature_selection/output/features_freq_selected.csv')
+    dataset_freq_inb = pd.read_csv('../feature_extraction/output/features_freq.csv')
+    dataset_bool_inb_s = pd.read_csv('../feature_selection/output/features_bool_selected.csv')
+    dataset_bool_inb = pd.read_csv('../feature_extraction/output/features_bool.csv')
+    
+    dataset_freq_bal_s = pd.read_csv('../feature_selection/output/features_freq_balanced_selected.csv')
+    dataset_freq_bal = pd.read_csv('../feature_extraction/output/features_freq_balanced.csv')
+    dataset_bool_bal_s = pd.read_csv('../feature_selection/output/features_bool_balanced_selected.csv')
+    dataset_bool_bal = pd.read_csv('../feature_extraction/output/features_bool_balanced.csv')
+    
     dataset_label = pd.read_csv('../feature_extraction/output/label.csv')['intent']
     dataset_label_bal = pd.read_csv('../feature_extraction/output/balanced_label.csv')['intent']
 
     print "[+] Training MNB models"
+    MNB_freq_inb_s = MultinomialNB().fit(dataset_freq_inb_s, dataset_label)
     MNB_freq_inb = MultinomialNB().fit(dataset_freq_inb, dataset_label)
+    MNB_bool_inb_s = MultinomialNB().fit(dataset_bool_inb_s, dataset_label)
     MNB_bool_inb = MultinomialNB().fit(dataset_bool_inb, dataset_label)
+    MNB_freq_bal_s = MultinomialNB().fit(dataset_freq_bal_s, dataset_label_bal)
     MNB_freq_bal = MultinomialNB().fit(dataset_freq_bal, dataset_label_bal)
+    MNB_bool_bal_s = MultinomialNB().fit(dataset_bool_bal_s, dataset_label_bal)
     MNB_bool_bal = MultinomialNB().fit(dataset_bool_bal, dataset_label_bal)
 
     print "[+] Training Decision Tree models"
+    tree_freq_inb_s = DecisionTreeClassifier().fit(dataset_freq_inb_s, dataset_label)
     tree_freq_inb = DecisionTreeClassifier().fit(dataset_freq_inb, dataset_label)
+    tree_bool_inb_s = DecisionTreeClassifier().fit(dataset_bool_inb_s, dataset_label)
     tree_bool_inb = DecisionTreeClassifier().fit(dataset_bool_inb, dataset_label)
+    tree_freq_bal_s = DecisionTreeClassifier().fit(dataset_freq_bal_s, dataset_label_bal)
     tree_freq_bal = DecisionTreeClassifier().fit(dataset_freq_bal, dataset_label_bal)
+    tree_bool_bal_s = DecisionTreeClassifier().fit(dataset_bool_bal_s, dataset_label_bal)
     tree_bool_bal = DecisionTreeClassifier().fit(dataset_bool_bal, dataset_label_bal)
 
     print "[+] Saving models to files"
+    pickle.dump(MNB_freq_inb_s, open('output/MNB_freq_inb_s.pkl', 'wb'))
     pickle.dump(MNB_freq_inb, open('output/MNB_freq_inb.pkl', 'wb'))
+    pickle.dump(MNB_bool_inb_s, open('output/MNB_bool_inb_s.pkl', 'wb'))
     pickle.dump(MNB_bool_inb, open('output/MNB_bool_inb.pkl', 'wb'))
+
+    pickle.dump(MNB_freq_bal_s, open('output/MNB_freq_bal_s.pkl', 'wb'))
     pickle.dump(MNB_freq_bal, open('output/MNB_freq_bal.pkl', 'wb'))
+    pickle.dump(MNB_bool_bal_s, open('output/MNB_bool_bal_s.pkl', 'wb'))
     pickle.dump(MNB_bool_bal, open('output/MNB_bool_bal.pkl', 'wb'))
+
+    pickle.dump(tree_freq_inb_s, open('output/tree_freq_inb_s.pkl', 'wb'))
     pickle.dump(tree_freq_inb, open('output/tree_freq_inb.pkl', 'wb'))
+    pickle.dump(tree_bool_inb_s, open('output/tree_bool_inb_s.pkl', 'wb'))
     pickle.dump(tree_bool_inb, open('output/tree_bool_inb.pkl', 'wb'))
+
+    pickle.dump(tree_freq_bal_s, open('output/tree_freq_bal_s.pkl', 'wb'))
     pickle.dump(tree_freq_bal, open('output/tree_freq_bal.pkl', 'wb'))
+    pickle.dump(tree_bool_bal_s, open('output/tree_bool_bal_s.pkl', 'wb'))
     pickle.dump(tree_bool_bal, open('output/tree_bool_bal.pkl', 'wb'))
 
 if __name__ == '__main__':
